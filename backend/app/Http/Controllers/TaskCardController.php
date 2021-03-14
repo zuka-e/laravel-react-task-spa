6efb078534c61cb8810b58f6d732af3ff4aca617
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-03-14 18:08:15 +0900

    Generate a migration, seeder, factory, and resource controller for the model of "TaskCard"

---
+++ backend/app/Http/Controllers/TaskCardController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TaskCard;
+use Illuminate\Http\Request;
+
+class TaskCardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TaskCard  $taskCard
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TaskCard $taskCard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TaskCard  $taskCard
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TaskCard $taskCard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TaskCard  $taskCard
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TaskCard $taskCard)
+    {
+        //
+    }
+}
---
+++ backend/app/Models/TaskCard.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaskCard extends Model
+{
+    use HasFactory;
+}
