83a1e8e497ceab4f5d4e2885d8f9ef3a752dcfe4
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-04-09 23:47:01 +0900

    Update the "home" route used after login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/home', fn () => Auth::user());
 
 Route::group([
     'namespace' => 'App\Http\Controllers',
