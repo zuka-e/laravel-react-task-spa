6c9ca374d945fea91b80624534beb1a3188e0d76
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-04-24 17:57:02 +0900

    Add the header leading to sign-in and sign-out features

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,6 +5,7 @@ import { APP_NAME } from '../config/app';
 import { isSignedIn } from '../utils/auth';
 import { useAppDispatch } from '../store/hooks';
 import { putSignOut } from '../store/slices/authSlice';
+import Header from '../layouts/Header';
 
 const Home: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -18,6 +19,7 @@ const Home: React.FC = () => {
       <Helmet>
         <title>{APP_NAME}</title>
       </Helmet>
+      <Header />
       {isSignedIn() && <Button onClick={handleClick}>Logout</Button>}
     </React.Fragment>
   );
