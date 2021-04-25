e3368551b8413da2b88fe74cf3cece074b34661c
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-04-26 07:02:52 +0900

    Update theme colors

--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -4,12 +4,16 @@ import typography from './typography';
 const theme = createMuiTheme({
   palette: {
     primary: {
+      light: '#e0fffa',
       main: '#40cbb5',
       contrastText: '#fff',
     },
     secondary: {
-      main: '#e0fffa',
+      main: '#ffa133',
+      contrastText: '#fff',
     },
+    // light, dark値の算出 0に近いほど main値に近付く (0-1)
+    tonalOffset: 0.025,
   },
   typography,
 });
