4fd70bb2beabef0db8ae1da7f6c72646737af109
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-04-27 21:42:47 +0900

    Add a Markdown component with a custom config

---
+++ frontend/src/templates/Markdown.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from 'react';
+import MarkdownToJsx, { MarkdownToJSX } from 'markdown-to-jsx';
+import { Typography } from '@material-ui/core';
+
+const options: MarkdownToJSX.Options = {
+  slugify: (str) => str, // 自動生成されるid属性を日本語で利用
+  overrides: {
+    h1: {
+      component: (props) => (
+        <Typography gutterBottom component='h1' variant='h3' {...props} />
+      ),
+    },
+    h2: {
+      component: (props) => (
+        <Typography gutterBottom component='h2' variant='h4' {...props} />
+      ),
+    },
+    h3: {
+      component: (props) => (
+        <Typography gutterBottom component='h3' variant='h5' {...props} />
+      ),
+    },
+    h4: {
+      component: (props) => (
+        <Typography gutterBottom component='h4' variant='h6' {...props} />
+      ),
+    },
+    p: { component: (props) => <Typography paragraph {...props} /> },
+    li: {
+      component: (props) => <Typography component='li' {...props} />,
+    },
+  },
+};
+
+const Markdown: React.FC = ({ children }) => {
+  return (
+    <MarkdownToJsx options={options}>
+      {children as string & ReactNode}
+    </MarkdownToJsx>
+  );
+};
+
+export default Markdown;
