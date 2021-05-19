448ed84d4b3bc90265fa03693454daab0e8c6480
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-05-20 08:31:53 +0900

    Configure and setup Service Worker in Node, `Jest`

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,3 +3,19 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { server } from './mocks/server';
+
+beforeAll(() => {
+  // Enable the mocking in tests.
+  server.listen();
+});
+
+afterEach(() => {
+  // Reset any runtime handlers tests may use.
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // Clean up once the tests are done.
+  server.close();
+});
