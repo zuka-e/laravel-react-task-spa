4d41f8df8bd222059af74806ccf295d477d87ad3
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-06-04 14:00:49 +0900

    Add JSDoc to some methods for testing

--- frontend/src/mocks/models/auth.ts
+++ frontend/src/mocks/models/auth.ts
@@ -7,14 +7,26 @@ const user = {} as UserDocument;
 
 const exists = () => Object.keys(user).length > 0;
 
+/**
+ * 認証ユーザー取得
+ */
 export const getUser = () => {
   const userRef = { ...user };
   return exists() ? userRef : null;
 };
 
+/**
+ * 認証ユーザーとして登録
+ */
 export const setUser = (userDoc: UserDocument) => Object.assign(user, userDoc);
 
+/**
+ * 認証ユーザーとして登録
+ */
 export const login = (userDoc: UserDocument) => setUser(userDoc);
 
+/**
+ * 登録した認証ユーザーを削除
+ */
 export const logout = () =>
   Object.keys(user).forEach((key) => delete user[key as keyof typeof user]);
--- frontend/src/mocks/models/index.ts
+++ frontend/src/mocks/models/index.ts
@@ -1,37 +1,89 @@
 import { UsersCollection } from './user';
 
+/**
+ * 継承した`interface`は動的プロパティ`Document`を持つ
+ */
+export interface Collection {
+  [uuid: string]: Document;
+}
+
+/**
+ * 継承した`interface`は必須プロパティを持つ
+ */
+export interface Document {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+/**
+ * 初期値: テスト毎に`localStorage`をこの状態にリセット
+ *
+ * @property count 各`Document`の作成回数 (`id`に使用)
+ * @property database `Collection`の集合
+ */
 const initilalState = {
   count: {
     users: 0,
   },
   database: {
     users: {} as UsersCollection,
-  } as const,
+  },
 };
 
 // `initilalState`が変更されないようにスプレッド演算子でコピー
 const count = { ...initilalState.count };
 const database = { ...initilalState.database };
 
+/**
+ * `Collection`の集合
+ */
 type DB = typeof database;
 
+/**
+ * 1. 指定された`key`を持つJSONデータを`localStorage`から取得
+ * 2. 指定された`key`を持つ`Collection`に取得したデータをコピー (上書き)
+ *
+ * @param  key - 各`Collection`に割り当てられたキー名
+ */
 const load = <K extends keyof DB>(key: K) => {
   const storedData = localStorage.getItem(key) || '{}';
   Object.assign(database[key], JSON.parse(storedData));
 };
 
+/**
+ * 指定された`Collection`をJSON変換して`localStorage`に保存
+ *
+ * @param  key - 各`Collection`に割り当てられたキー名
+ */
 const save = <K extends keyof DB>(key: K) => {
   localStorage.setItem(key, JSON.stringify(database[key]));
 };
 
+/**
+ * 指定された`Collection`の`Document`の存在有無を確認
+ *
+ * @param  key - 各`Collection`に割り当てられたキー名
+ */
 const exists = <K extends keyof DB>(key: K) =>
   Object.keys(database[key]).length > 0;
 
+/**
+ * 指定された`Collection`のコピーを返却
+ *
+ * @param  key - 各`Collection`に割り当てられたキー名
+ */
 const collection = <K extends keyof DB>(key: K) => {
   const collectionClone = { ...database[key] };
   return collectionClone; // 参照のみ許可 (直接データを返さない)
 };
 
+/**
+ * 指定された`Collection`に引数の`Document`を新たに作成
+ *
+ * @param  key - 各`Collection`に割り当てられたキー名
+ * @param  doc - `Document`
+ */
 const create = <K extends keyof DB, T extends DB[K]>(key: K, doc: T['id']) => {
   const defaultValues = {
     id: count[key] + 1,
@@ -61,6 +113,12 @@ const where = <K extends keyof DB, T extends DB[K]>(
     (doc) => doc[column as keyof typeof doc] === value
   );
 
+/**
+ * 指定された`Collection`の`Document`を更新
+ *
+ * @param  key - 各`Collection`に割り当てられたキー名
+ * @param  doc - `Document`
+ */
 const update = <K extends keyof DB, T extends DB[K]>(key: K, doc: T['id']) => {
   const uuid = String(doc.id);
   const newState = { ...database[key], [uuid]: doc };
@@ -69,6 +127,11 @@ const update = <K extends keyof DB, T extends DB[K]>(key: K, doc: T['id']) => {
   save(key);
 };
 
+/**
+ * 指定された`Collection`の`Document`を初期化 (指定しない場合、全ての`Collection`が対象)
+ *
+ * @param  key - 各`Collection`に割り当てられたキー名
+ */
 const reset = <K extends keyof DB>(key?: K) => {
   if (key) {
     database[key] = initilalState.database[key];
@@ -79,6 +142,9 @@ const reset = <K extends keyof DB>(key?: K) => {
   }
 };
 
+/**
+ * データを操作する各メソッドをプロパティとして持つ
+ */
 export const db = {
   load,
   exists,
