e0908f06924c03f12a3fc8c1a6215b267ca77e5d
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-07-03 11:58:33 +0900

    Add links within a page as a breadcrumbs trail

--- frontend/src/components/boards/TaskBoard/TaskCardDetails.tsx
+++ frontend/src/components/boards/TaskBoard/TaskCardDetails.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import moment from 'moment';
+import { useParams } from 'react-router-dom';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import {
   Box,
@@ -13,15 +14,20 @@ import {
   Checkbox,
   IconButton,
   Typography,
+  Breadcrumbs,
 } from '@material-ui/core';
-import { Close as CloseIcon } from '@material-ui/icons';
+import {
+  Close as CloseIcon,
+  ListAlt as ListAltIcon,
+  Assignment as AssignmentIcon,
+} from '@material-ui/icons';
 import { KeyboardDateTimePicker } from '@material-ui/pickers';
 import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
 
 import theme from 'theme';
 import { TaskCard } from 'models';
 import { closeInfoBox, removeInfoBox } from 'store/slices/taskBoardSlice';
-import { useAppDispatch } from 'utils/hooks';
+import { useAppDispatch, useAppSelector } from 'utils/hooks';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -31,7 +37,14 @@ const useStyles = makeStyles((theme: Theme) =>
       height: '100%',
       borderRadius: 0,
     },
+    breadcrumbs: { '& li > *': { display: 'flex', alignItems: 'center' } },
+    icon: {
+      marginRight: theme.spacing(0.5),
+      width: 20,
+      height: 20,
+    },
     close: { marginLeft: 'auto' },
+    cardHeader: { paddingBottom: 0 },
     rows: {
       '& > div': {
         marginBottom: theme.spacing(1),
@@ -39,8 +52,8 @@ const useStyles = makeStyles((theme: Theme) =>
       },
     },
     label: { flex: '0 0 100px' },
-    text: { whiteSpace: 'pre-wrap' },
     timeout: { color: theme.palette.error.main },
+    text: { whiteSpace: 'pre-wrap' },
   })
 );
 
@@ -51,7 +64,12 @@ type TaskCardDetailsProps = {
 const TaskCardDetails: React.FC<TaskCardDetailsProps> = (props) => {
   const { card } = props;
   const classes = useStyles();
+  const params = useParams<{ userId: string; boardId: string }>();
   const dispatch = useAppDispatch();
+  const state = useAppSelector((state) => state.boards);
+  const board = state.docs[params.boardId];
+  const list = board.lists.find((list) => list.id === card.listId);
+  const baseUrl = `${window.location.origin}${window.location.pathname}`;
 
   const isInTime = (date: Date) => moment(date).isBefore(new Date(), 'minute');
 
@@ -59,7 +77,6 @@ const TaskCardDetails: React.FC<TaskCardDetailsProps> = (props) => {
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     e.preventDefault();
-    // toggle `done`
   };
 
   const handleClose = () => {
@@ -81,6 +98,34 @@ const TaskCardDetails: React.FC<TaskCardDetailsProps> = (props) => {
   return (
     <Card className={classes.root}>
       <CardActions disableSpacing>
+        <Breadcrumbs aria-label='breadcrumb' className={classes.breadcrumbs}>
+          <a href={`${baseUrl}#${list?.id}`}>
+            <ListAltIcon className={classes.icon} />
+            {list?.title}
+          </a>
+          <Typography>
+            <AssignmentIcon className={classes.icon} />
+            {'Card'}
+          </Typography>
+        </Breadcrumbs>
+        <IconButton
+          aria-label='close'
+          onClick={handleClose}
+          size='small'
+          className={classes.close}
+        >
+          <CloseIcon />
+        </IconButton>
+      </CardActions>
+      <CardHeader
+        className={classes.cardHeader}
+        title={
+          <Typography className={classes.text} variant='h5' component='p'>
+            {card.title}
+          </Typography>
+        }
+      />
+      <CardContent className={classes.rows}>
         <FormControlLabel
           control={
             <Checkbox
@@ -91,22 +136,6 @@ const TaskCardDetails: React.FC<TaskCardDetailsProps> = (props) => {
           }
           label={card.done ? 'Completed' : 'Incompleted'}
         />
-        <IconButton
-          aria-label='close'
-          onClick={handleClose}
-          className={classes.close}
-        >
-          <CloseIcon />
-        </IconButton>
-      </CardActions>
-      <CardHeader
-        title={
-          <Typography className={classes.text} variant='h5' component='p'>
-            {card.title}
-          </Typography>
-        }
-      />
-      <CardContent className={classes.rows}>
         <Grid container>
           <Grid item className={classes.label}>
             <label className={isInTime(card.deadline) ? classes.timeout : ''}>
