c465fb877ede5b30208b8bb12c494266a4b9bfd1
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-07-13 22:42:14 +0900

    Enable the API client to be used for thunks for auth
    for less duplication

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,12 +1,11 @@
 import axios, { AxiosError } from 'axios';
 
 import { API_HOST, API_VERSION } from 'config/api';
-import store from 'store';
-import { setFlash, signOut } from 'store/slices/authSlice';
 
-export const apiClient = (params?: { nonApiRoute: true }) => {
-  const nonApiRoute = !!params?.nonApiRoute; // 引数なし -> false
-  const baseURL = nonApiRoute ? API_HOST : API_HOST + '/api/' + API_VERSION;
+export const apiClient = (options?: { apiRoute?: boolean }) => {
+  /** @returns `true` even if `apiRoute` is `undefined` */
+  const isNonApiRoute = () => options?.apiRoute === false;
+  const baseURL = API_HOST + (isNonApiRoute() ? '' : '/api/' + API_VERSION);
   const apiClient = axios.create({
     baseURL: baseURL,
     withCredentials: true,
@@ -14,7 +13,10 @@ export const apiClient = (params?: { nonApiRoute: true }) => {
 
   apiClient.interceptors.response.use(
     (response) => response, // response = 2xx の場合は素通り
-    (err) => {
+    async (err) => {
+      const { default: store } = await import('store');
+      const { setFlash, signOut } = await import('store/slices/authSlice');
+
       const error = err as AxiosError;
       const statuscode = error.response?.status || 500;
 
@@ -22,6 +24,9 @@ export const apiClient = (params?: { nonApiRoute: true }) => {
         case 401:
         case 419:
           store.dispatch(signOut()); // ->initializeAuthState()
+          store.dispatch(
+            setFlash({ type: 'error', message: 'ログインしてください' })
+          );
           return Promise.reject(error);
         case 403:
           store.dispatch(
