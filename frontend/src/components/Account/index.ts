f85c432d32c37d8eb69bb23bbde292165c9a6a99
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-07-17 03:14:47 +0900

    Fix rename operation

--- frontend/src/components/Account/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DeleteAccountDialog } from './DeleteAccountDialog';
-export { default as Password } from './Password';
-export { default as UserProfile } from './UserProfile';
-export { default as UserStatus } from './UserStatus';
