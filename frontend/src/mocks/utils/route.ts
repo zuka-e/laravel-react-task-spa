21014080e20fc0e0eb39b93e99d5ecf8a65e9b97
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-07-20 01:54:28 +0900

    Add a function to set the path for msw

--- frontend/src/mocks/utils/route.ts
+++ frontend/src/mocks/utils/route.ts
@@ -5,3 +5,20 @@ export const url = (pathName: keyof typeof paths) => {
   const nonApiRouteList = [GET_CSRF_TOKEN_PATH];
   return nonApiRouteList.includes(path) ? API_HOST + path : API_ROUTE + path;
 };
+
+/** パスを構成する要素の内、モデル(テーブル)名として使用される文字列 */
+const tableNames = ['users', 'task_boards'] as const;
+
+/** `tableName`と`id`(省略可)との組み合わせ */
+type PathSet = [table: typeof tableNames[number], id?: string];
+
+/** `PathSet`の配列(可変長)を連結してパスを作成 */
+export const makePath = (...props: PathSet[]) => {
+  const reducer = (acc: string, current: typeof props[0]) => {
+    const table = current[0];
+    const id = current[1];
+    return acc + `/${table}` + (id ? `/${id}` : '');
+  };
+  const path = props.reduce(reducer, '');
+  return path;
+};
