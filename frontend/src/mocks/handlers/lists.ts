205ca27475a09ef61ed8da6f0076f402ecc9fcf9
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-07-29 23:40:55 +0900

    Update MSW handlers and Add tests for a thunk for deleting `TaskList`

--- frontend/src/mocks/handlers/lists.ts
+++ frontend/src/mocks/handlers/lists.ts
@@ -1,4 +1,4 @@
-import { rest } from 'msw';
+import { DefaultRequestBody, rest } from 'msw';
 
 import { API_ROUTE } from 'config/api';
 import { makePath } from 'utils/api';
@@ -7,6 +7,7 @@ import {
   CreateTaskListResponse,
   UpdateTaskListRequest,
   UpdateTaskListResponse,
+  DestroyTaskListResponse,
 } from 'store/thunks/lists';
 import { db } from 'mocks/models';
 import {
@@ -77,6 +78,40 @@ export const handlers = [
       if (!updated) return res(ctx.status(404));
 
       return res(ctx.status(200), ctx.json({ data: updated }));
+    }
+  ),
+
+  rest.delete<DefaultRequestBody, DestroyTaskListResponse, TaskListParams>(
+    API_ROUTE +
+      makePath(['task_boards', ':boardId'], ['task_lists', ':listId']),
+    (req, res, ctx) => {
+      const currentUser = getUserFromSession(req.cookies.session_id);
+      const token = req.headers.get(X_XSRF_TOKEN);
+      const belongsTo = {
+        board: db.where('taskBoards', 'id', req.params.boardId)[0],
+      };
+      const target = db.where('taskLists', 'id', req.params.listId)[0];
+
+      if (!currentUser) return res(ctx.status(401));
+
+      if (!token || !hasValidToken(token)) return res(ctx.status(419));
+
+      if (!currentUser.emailVerifiedAt) return res(ctx.status(403));
+
+      if (!belongsTo.board) return res(ctx.status(404));
+
+      if (belongsTo.board.userId !== currentUser.id)
+        return res(ctx.status(403));
+
+      if (!target) return res(ctx.status(404));
+
+      if (target.boardId !== req.params.boardId) return res(ctx.status(403));
+
+      const deleted = taskListController.destroy(req);
+
+      if (!deleted) return res(ctx.status(404));
+
+      return res(ctx.status(200), ctx.json({ data: deleted }));
     }
   ),
 ];
