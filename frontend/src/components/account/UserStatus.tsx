151a0a9ebe22b90d66b5b13f0000570aee52d10c
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-08-18 00:39:54 +0900

    Remove unnecessary code

--- frontend/src/components/account/UserStatus.tsx
+++ frontend/src/components/account/UserStatus.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Fragment } from 'react';
 
 import { Box, Button, Grid } from '@material-ui/core';
 
@@ -7,27 +7,26 @@ import { useAppDispatch } from 'utils/hooks';
 import { isVerified } from 'utils/auth';
 import { AlertMessage } from 'templates';
 
-const UserStatus: React.FC = () => {
+const UserStatus = () => {
   const dispatch = useAppDispatch();
 
   const handleClick = () => {
     dispatch(sendEmailVerificationLink());
   };
 
-  const showEmailVerificationState = () => {
-    return isVerified() ? (
+  const showEmailVerificationState = () =>
+    isVerified() ? (
       <AlertMessage severity='success' body='認証済みです' />
     ) : (
-      <React.Fragment>
+      <Fragment>
         <AlertMessage severity='warning' body='メール認証が必要です' />
         <Box mt={3} mb={1}>
           <Button variant='contained' color='secondary' onClick={handleClick}>
             メールを再送信する
           </Button>
         </Box>
-      </React.Fragment>
+      </Fragment>
     );
-  };
 
   return (
     <Grid container>
