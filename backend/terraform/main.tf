4712488fdabe25d4b9429da4093dd70fc6df3fb8
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-09-13 12:35:02 +0900

    Fix the hard-coded aws profile settings

--- backend/terraform/main.tf
+++ backend/terraform/main.tf
@@ -1,10 +1,12 @@
 # https://learn.hashicorp.com/tutorials/terraform/locals?in=terraform/configuration-language
 
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
 provider "aws" {
-  profile = "default"
+  profile = local.profile
   region  = local.region
 }
 
 locals {
-  region = var.aws_region
+  profile = var.aws_profile
+  region  = var.aws_region
 }
