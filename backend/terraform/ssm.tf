5f5ad974502d225a2818097cebdbced910cead50
zuka-e <52391918+zuka-e@users.noreply.github.com>
2021-09-13 13:02:01 +0900

    Add SSM parameter for a security group and subnets

--- backend/terraform/ssm.tf
+++ backend/terraform/ssm.tf
@@ -51,6 +51,24 @@ resource "aws_ssm_parameter" "smtp_password" {
 }
 
 ################################################################################
+# VPC
+################################################################################
+
+resource "aws_ssm_parameter" "security_group_id" {
+  name        = "/${var.project}/${var.stage}/SECURITY_GROUP_ID"
+  type        = "SecureString"
+  value       = module.vote_service_sg.security_group_id
+  description = "ID of security group"
+}
+
+resource "aws_ssm_parameter" "private_subnets" {
+  name        = "/${var.project}/${var.stage}/PRIVATE_SUBNET_IDS"
+  type        = "StringList"
+  value       = join(",", module.vpc.private_subnets)
+  description = "List of IDs of private subnets"
+}
+
+################################################################################
 # RDS
 ################################################################################
 # https://github.com/terraform-aws-modules/terraform-aws-rds/blob/master/outputs.tf
