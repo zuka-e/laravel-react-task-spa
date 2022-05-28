2e010429a4a2caa6c01b8f1aa403622921bed93f
zuka-e <52391918+zuka-e@users.noreply.github.com>
2022-05-29 01:17:49 +0900

    Update deprecated Terraform parameters

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,18 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "main" {
   bucket = "${lower(var.project)}-${var.stage}"
-  acl    = "private"
+}
 
-  versioning {
-    enabled = true
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_acl
+resource "aws_s3_bucket_acl" "main" {
+  bucket = aws_s3_bucket.main.id
+  acl    = "private"
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_versioning
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
