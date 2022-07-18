0ab6cdbb0167ac74d6a0ee445db0ba8b82195b71
zuka-e <52391918+zuka-e@users.noreply.github.com>
2022-07-18 07:07:09 +0000

    Add type hinting to API resources

--- backend/app/Http/Resources/TaskBoardResource.php
+++ backend/app/Http/Resources/TaskBoardResource.php
@@ -2,30 +2,51 @@
 
 namespace App\Http\Resources;
 
+use App\Models\TaskBoard;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * It's used to transform the model and define what data should be returned.
+ *
+ * @see https://laravel.com/docs/9.x/eloquent-resources
+ */
 class TaskBoardResource extends JsonResource
 {
+    /** @var \App\Models\TaskBoard */
+    public $resource; // Type declaration can't be used
+
+    /** @param  \App\Models\TaskBoard  $resource */
+    public function __construct(TaskBoard $resource)
+    {
+        $this->resource = $resource;
+    }
+
     /**
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return array
+     * @return array<string, mixed>
      */
     public function toArray($request)
     {
+        // Undefined properties can be accessed by `__get()`.
+        // (Illuminate\Http\Resources\DelegatesToResource::__get)
+        // But it's not type-safe. So access via a typed variable.
+        // e.g. `$this->resource->id` instead of `this->id`
+        $taskBoard = $this->resource;
+
         return [
-            'id' => $this->id,
-            'userId' => $this->user_id,
-            'title' => $this->title,
-            'description' => $this->description,
+            'id' => $taskBoard->id,
+            'userId' => $taskBoard->user_id,
+            'title' => $taskBoard->title,
+            'description' => $taskBoard->description,
             'lists' => TaskListResource::collection(
                 $this->whenLoaded('taskLists'),
             ),
-            'createdAt' => $this->created_at,
-            'updatedAt' => $this->updated_at,
-            'listIndexMap' => $this->list_index_map,
-            'cardIndexMap' => $this->card_index_map,
+            'createdAt' => $taskBoard->created_at,
+            'updatedAt' => $taskBoard->updated_at,
+            'listIndexMap' => $taskBoard->list_index_map,
+            'cardIndexMap' => $taskBoard->card_index_map,
         ];
     }
 }
--- backend/app/Http/Resources/TaskCardResource.php
+++ backend/app/Http/Resources/TaskCardResource.php
@@ -2,28 +2,49 @@
 
 namespace App\Http\Resources;
 
+use App\Models\TaskCard;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * It's used to transform the model and define what data should be returned.
+ *
+ * @see https://laravel.com/docs/9.x/eloquent-resources
+ */
 class TaskCardResource extends JsonResource
 {
+    /** @var \App\Models\TaskCard */
+    public $resource; // Type declaration can't be used
+
+    /** @param  \App\Models\TaskCard  $resource */
+    public function __construct(TaskCard $resource)
+    {
+        $this->resource = $resource;
+    }
+
     /**
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return array
+     * @return array<string, mixed>
      */
     public function toArray($request)
     {
+        // Undefined properties can be accessed by `__get()`.
+        // (Illuminate\Http\Resources\DelegatesToResource::__get)
+        // But it's not type-safe. So access via a typed variable.
+        // e.g. `$this->resource->id` instead of `this->id`
+        $taskCard = $this->resource;
+
         return [
-            'id' => $this->id,
-            'userId' => $this->user_id,
-            'listId' => $this->task_list_id,
-            'title' => $this->title,
-            'content' => $this->content,
-            'deadline' => $this->deadline,
-            'done' => $this->done,
-            'createdAt' => $this->created_at,
-            'updatedAt' => $this->updated_at,
+            'id' => $taskCard->id,
+            'userId' => $taskCard->user_id,
+            'listId' => $taskCard->task_list_id,
+            'title' => $taskCard->title,
+            'content' => $taskCard->content,
+            'deadline' => $taskCard->deadline,
+            'done' => $taskCard->done,
+            'createdAt' => $taskCard->created_at,
+            'updatedAt' => $taskCard->updated_at,
         ];
     }
 }
