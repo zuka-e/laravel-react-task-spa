335b71d9bce8fc0d299a95c6f5fd5bda773a967b
zuka-e <52391918+zuka-e@users.noreply.github.com>
2022-07-19 15:14:15 +0000

    Move task controllers to a version 1 directory

--- backend/app/Http/Controllers/V1/TaskCardController.php
+++ backend/app/Http/Controllers/V1/TaskCardController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\V1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTaskCardRequest;
 use App\Http\Requests\UpdateTaskCardRequest;
 use App\Http\Resources\TaskCardResource;
--- backend/routes/api/v1.php
+++ backend/routes/api/v1.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\V1;
 
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
