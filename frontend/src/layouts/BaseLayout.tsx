1da30a0e55db75b423b059b4619fb569186282e2
zuka-e <52391918+zuka-e@users.noreply.github.com>
2022-08-11 05:40:50 +0900

    Replace `Helmet` component with `Head` of `next/head`

--- frontend/src/layouts/BaseLayout.tsx
+++ frontend/src/layouts/BaseLayout.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from 'react';
-
-import { Helmet } from 'react-helmet-async';
+import Head from 'next/head';
 
 import { APP_NAME } from 'config/app';
 import { Header, Footer } from 'layouts';
@@ -9,11 +8,11 @@ type BaseLayoutProps = { subtitle: string; withoutHeaders?: boolean };
 
 const BaseLayout: React.FC<BaseLayoutProps> = (props) => (
   <Fragment>
-    <Helmet>
+    <Head>
       <title>
         {props.subtitle ? `${props.subtitle} | ${APP_NAME}` : APP_NAME}
       </title>
-    </Helmet>
+    </Head>
     {!props.withoutHeaders && <Header />}
     {props.children}
     {!props.withoutHeaders && <Footer />}
