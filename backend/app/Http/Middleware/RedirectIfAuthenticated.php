487ae39394861d16e15b21dae2407bae4c732259
zuka-e <52391918+zuka-e@users.noreply.github.com>
2022-10-06 23:38:55 +0000

    Fix not returning the user in response to a login request with logged in

--- backend/app/Http/Middleware/RedirectIfAuthenticated.php
+++ backend/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -23,12 +22,7 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                if ($request->expectsJson()) {
-                    return response()->json([
-                        'error' => ['message' => 'Already authenticated.'],
-                    ]);
-                }
-                return redirect(RouteServiceProvider::HOME);
+                return redirect()->intended(config('fortify.home'));
             }
         }
 
